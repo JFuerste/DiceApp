05be8aa4fea18cff63f5ec31c4903f50cea141dd
Fürste <joshua.fuerste@rwth-aachen.de>
2020-04-02 17:44:18 +0200

    Add Images and button functionality

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.diceapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.ImageView
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +13,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.text = "Let's Roll"
-        rollButton.setOnClickListener { Toast.makeText(this, "Rolled the dice", Toast.LENGTH_SHORT).show() }
-
+        rollButton.setOnClickListener { rollDice() }
     }
+
+    private fun rollDice() {
+        val resultImg: ImageView = findViewById(R.id.result_img)
+        val randomValue = Random().nextInt(6) + 1
+        val drawableResource = when (randomValue) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        resultImg.setImageResource(drawableResource)
+    }
+
 }
