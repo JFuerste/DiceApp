bb0aba4ee26e13d9474b7efd4ec283b9e60d1e8c
Fürste <joshua.fuerste@rwth-aachen.de>
2020-04-02 17:35:05 +0200

    Add button and result text

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.diceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener { Toast.makeText(this, "Rolled the dice", Toast.LENGTH_SHORT).show() }
+
     }
 }
